8a9a19fbf20c1696c2978cffb1af3200553a0feb
amal ziad <git config --global user.email>
2022-11-04 15:12:12 +0200

    Initial commit

---
+++ src/main/java/com/example/invotoryservice/InvotoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.invotoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvotoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvotoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/invotoryservice/InvotoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invotoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvotoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
